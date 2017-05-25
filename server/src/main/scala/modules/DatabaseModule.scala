bdb81d01f6ed54eb886410ddbdcff9415db6aca2
Riddhi Sharma <riddhi041994@gmail.com>
2017-05-25 10:11:48 +0530

    Adding files for the new app.

---
+++ server/src/main/scala/modules/DatabaseModule.scala
@@ -0,0 +1,18 @@
+package modules
+
+import com.softwaremill.macwire.wire
+import io.getquill.{PostgresAsyncContext, SnakeCase}
+import services.UniversityService
+
+import scala.concurrent.ExecutionContext
+
+/**
+ * Created by shubham on 24/5/17.
+ */
+trait DatabaseModule {
+  // quill
+  implicit def executionContext: ExecutionContext
+  // database
+  lazy val dbCtx = new PostgresAsyncContext[SnakeCase]("ctx")
+  lazy val universityService = wire[UniversityService]
+}
